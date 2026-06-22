9ed2185eb2355f63b826825de40e8b85da1d78f4
bhagiradhijayaram <bhagiradhijayaram188@gmail.com>
2026-06-22 12:31:06 +0530

    Ready to Deployment

--- web-client/src/features/home/pages/HomePage.jsx
+++ web-client/src/features/home/pages/HomePage.jsx
@@ -2,7 +2,7 @@ import Banner from "../sections/Banner";
 import Contact from "../sections/Contact";
 import GlobalTrade from "../sections/GlobalTrade";
 import HeroSection from "../sections/HeroSection";
-import OurFeaturedProducts from "../sections/ourFeaturedProducts";
+import OurFeaturedProducts from "../sections/OurFeaturedProducts";
 import OurProcess from "../sections/OurProcess";
 import WhyChoose from "../sections/WhyChoose";
 
